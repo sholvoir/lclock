ccf4f53252fc765d08f1b3dd159dfd533f980fb0
sholvoir <sovar.he@gmail.com>
2024-08-26 22:30:24 -0400

    movement

--- islands/clock.tsx
+++ islands/clock.tsx
@@ -1,6 +1,13 @@
 import { Signal } from "@preact/signals";
 import { ebtext, ebdtext, quartertext, dititalCn } from "../lib/eb.ts";
 
+const [r1, r2, r3, r4, r5] = [120, 424, 436, 498, 510];
+const tquart = 96;
+const tsecond = 180;
+const bhour = 12;
+const longScale = 12;
+const shortScale = 6;
+
 const clockCoor2SvgCoor = ([radius, angleCents]: [number, number]) => {
     const radian = 2 * Math.PI * angleCents;
     return [radius * Math.sin(radian), -radius * Math.cos(radian)];
@@ -8,50 +15,53 @@ const clockCoor2SvgCoor = ([radius, angleCents]: [number, number]) => {
 
 const quarterLine = () => {
     const result = [];
-    for (let i = 0; i < 48; i++) {
-        const [x1, y1] = clockCoor2SvgCoor([424, i/48]);
-        const [x2, y2] = clockCoor2SvgCoor([436, i/48]);
+    const h = tquart / 2
+    for (let i = 0; i < h; i++) {
+        const [x1, y1] = clockCoor2SvgCoor([r3-longScale, i/h]);
+        const [x2, y2] = clockCoor2SvgCoor([r3, i/h]);
         result.push(<line x1={x1} y1={y1} x2={x2} y2={y2}/>);
     }
-    for (let i = 0; i < 48; i++) {
-        const [x1, y1] = clockCoor2SvgCoor([430, i/48+1/96]);
-        const [x2, y2] = clockCoor2SvgCoor([436, i/48+1/96]);
+    for (let i = 0; i < h; i++) {
+        const [x1, y1] = clockCoor2SvgCoor([r3-shortScale, i/h+1/tquart]);
+        const [x2, y2] = clockCoor2SvgCoor([r3, i/h+1/tquart]);
         result.push(<line x1={x1} y1={y1} x2={x2} y2={y2}/>);
     }
     return result;
 }
 const secondLine = () => {
     const result = [];
-    for (let i = 0; i < 90; i++) {
-        const [x1, y1] = clockCoor2SvgCoor([500, i/90]);
-        const [x2, y2] = clockCoor2SvgCoor([512, i/90]);
+    const h = tsecond/2;
+    for (let i = 0; i < h; i++) {
+        const [x1, y1] = clockCoor2SvgCoor([r5-longScale, i/h]);
+        const [x2, y2] = clockCoor2SvgCoor([r5, i/h]);
         result.push(<line x1={x1} y1={y1} x2={x2} y2={y2}/>);
     }
-    for (let i = 0; i < 90; i++) {
-        const [x1, y1] = clockCoor2SvgCoor([506, i/90+1/180]);
-        const [x2, y2] = clockCoor2SvgCoor([512, i/90+1/180]);
+    for (let i = 0; i < h; i++) {
+        const [x1, y1] = clockCoor2SvgCoor([r5-shortScale, i/h+1/tsecond]);
+        const [x2, y2] = clockCoor2SvgCoor([r5, i/h+1/tsecond]);
         result.push(<line x1={x1} y1={y1} x2={x2} y2={y2}/>);
     }
     return result;
 }
 const dizhiText = () => {
-    const result = []
-    for (let i = 0; i < 12; i++) {
-        const [x, y] = clockCoor2SvgCoor([470, i/12]);
+    const result = [];
+    const r = (r3+r4)/2;
+    for (let i = 0; i < bhour; i++) {
+        const [x, y] = clockCoor2SvgCoor([r, i/bhour]);
         result.push(<text x={x} y={y} alignment-baseline="central" transform-origin={`${x} ${y}`} transform={`rotate(${i*30})`}>{ebtext[i]}</text>);
     }
     return result;
 }
 const bhourLine = (second: number) => {
     const remain = second%86400/86400;
-    const [x1, y1] = clockCoor2SvgCoor([90, remain])
-    const [x2, y2] = clockCoor2SvgCoor([420, remain]);
+    const [x1, y1] = clockCoor2SvgCoor([r1, remain])
+    const [x2, y2] = clockCoor2SvgCoor([r2-2, remain]);
     return <line x1={x1} y1={y1} x2={x2} y2={y2}/>;
 }
 const bsecondLine = (second: number) => {
     const remain = second%86400%3600%900/900;
-    const [x1, y1] = clockCoor2SvgCoor([90, remain])
-    const [x2, y2] = clockCoor2SvgCoor([511, remain]);
+    const [x1, y1] = clockCoor2SvgCoor([r1, remain])
+    const [x2, y2] = clockCoor2SvgCoor([r4-2, remain]);
     return <line x1={x1} y1={y1} x2={x2} y2={y2}/>;
 }
 const totalText = (second: number) => {
@@ -64,7 +74,7 @@ const totalText = (second: number) => {
     remain %= 100;
     const shi = Math.floor(remain/10);
     remain %= 10;
-    return <g x="0" y="0" font-size="36" text-anchor="middle">
+    return <g x="0" y="0" font-size="48" text-anchor="middle">
         <text alignment-baseline="text-after-edge">{ebdtext[hour]}{quartertext[quarter]}</text>
         <text alignment-baseline="text-before-edge">{dititalCn[bai]}{dititalCn[shi]}{dititalCn[remain]}</text>;
     </g>
@@ -74,7 +84,7 @@ interface IClockProps {
     second: Signal<number>;
 }
 export default ({second}: IClockProps) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="-512 -512 1024 1024" fill="currentcolor" stroke="currentcolor">
-    <g fill="none" stroke-width="2">{[511, 500, 436, 424, 90].map(r => <circle r={r}/>)}</g>
+    <g fill="none" stroke-width="2">{[r1,r2,r3,r4,r5].map(r => <circle r={r}/>)}</g>
     <g font-size="48" text-anchor="middle">{dizhiText()}</g>
     <g stroke-width="2">{quarterLine()}{secondLine()}</g>
     <g stroke-width="2">{bhourLine(second.value)}{bsecondLine(second.value)}</g>
--- islands/root.tsx
+++ islands/root.tsx
@@ -4,19 +4,15 @@ import Clock from "./clock.tsx";
 
 let timediff = -4*60*60;
 const timeConvert = (second: number) => Math.round(second + timediff);
-
+let i = 0;
 
 export default () => {
     const second = useSignal(timeConvert(Date.now()/1000));
     useEffect(() => {
         const interval = setInterval(() => {
             second.value = timeConvert(Date.now()/1000);
+            if (i++%300==0) navigator.geolocation.getCurrentPosition(position => timediff = 60 * 60 * position.coords.longitude / 15);
         }, 1000);
-        navigator.geolocation.getCurrentPosition((position) => {
-            console.log(position.coords);
-            timediff = (60 * 60) * position.coords.longitude / 15;
-            console.log(timediff, -4*60*60);
-        });
         return () => clearInterval(interval);
     }, []);
     return <Clock second={second}/>
