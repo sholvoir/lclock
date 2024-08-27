b7bb7b52ada508f31ab44d8dc0e90ff96d717ff7
sholvoir <sovar.he@gmail.com>
2024-08-27 14:38:13 -0400

    NaN issue

--- islands/root.tsx
+++ islands/root.tsx
@@ -4,25 +4,18 @@ import Clock from "./clock.tsx";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 
 let i = 0;
-const getCoords = (): GeolocationCoordinates => {
-    const coords = localStorage.getItem('_coords');
-    return coords ? JSON.parse(coords) : { latitude: 0, longitude: 0, accuracy: 0 }
-}
-
-const setCoords = (coords: GeolocationCoordinates) => {
-    localStorage.setItem('_coords', JSON.stringify(coords));
-}
-
-const timeConvert = (second: number) =>
-    Math.round(second + 240 * getCoords().longitude);
 
 export default () => {
     if (!IS_BROWSER) return <svg/>
+    const longitude = useSignal(0);
+    const timeConvert = (second: number) => Math.round(second + 240 * longitude.value);
     const second = useSignal(timeConvert(Date.now()/1000));
     useEffect(() => {
         const interval = setInterval(() => {
             second.value = timeConvert(Date.now()/1000);
-            if (i++%300==0) navigator.geolocation.getCurrentPosition(position => setCoords(position.coords));
+            if (i++%300==0) navigator.geolocation.getCurrentPosition(position => {
+                if (position.coords.longitude !== null) longitude.value = position.coords.longitude;
+            });
         }, 1000);
         return () => clearInterval(interval);
     }, []);
